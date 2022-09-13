6aa764800acdac54f451c1d34edb51f71b7a3d9a
Derek White <derekdwhite25@gmail.com>
2022-09-13 14:06:58 -0700

    edited Derek.sh to fulfill assignment specifications

--- Derek.sh
+++ Derek.sh
@@ -2,3 +2,12 @@
 # then check if a directory called TXTBACKUP exists or not
 # if not create that dir and then zip/tar all the .txt files in the current directory and write the 
 # archived file to the new TXTBACKUP directory
+
+#!/bin/bash
+
+echo "Derek White"
+
+if [ ! -d "/TXTBACKUP" ]; then
+	mkdir TXTBACKUP
+	zip TXTBACKUP/Derek.zip *.sh
+fi
