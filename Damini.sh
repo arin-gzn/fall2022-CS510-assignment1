c62e0ef532362482a2889e97e4db281c9f582869
Damini Trehan <damini.trehan@gmail.com>
2022-09-14 17:14:57 -0700

    This is Damini Trehan's first homework assignment for CS 510

--- Damini.sh
+++ Damini.sh
@@ -2,3 +2,9 @@
 # then create a file called myreadonly.txt using touch command
 # then store the results of ls -l . command in this file
 # then make this file readonly using chmod command
+
+#!/bin/sh
+echo "Damini Trehan"
+touch myreadonly.txt
+echo ls-l > myreadonly.txt
+chmod u=rw,g=,o= myreadonly.txt
