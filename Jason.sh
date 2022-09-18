0c8053fa63cf25c2aafd5dd4765073a04ab09db5
jasonminbaik <jasonminbaik@gmail.com>
2022-09-18 13:45:19 -0700

    Jason's Commit

--- Jason.sh
+++ Jason.sh
@@ -4,6 +4,18 @@
 # touch the new file
 # show the first 10 lines of the file on the screen
 
-echo Jason Baik
-echo "Hello Class!"
+# print name & Hello class at the top of a file
+sed '1 i echo Jason Baik\' Jason.sh
+sed '1 i echo Hello Class\' Jason.sh
+
+# copy file to newdest directory
+mkddir newdest
+cp ~Jason.sh newdest
+
+# touch new file
+touch Jason.sh
+
+# show first 10 lines of file to the screen
+head -10 Jason.sh
+
 
