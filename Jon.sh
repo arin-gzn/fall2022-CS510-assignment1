ca63f2b62267c6811104c21fa5fa7f33b765c963
Jon Flees <jonflees@gmail.com>
2022-09-18 20:06:41 -0700

    Jon Flees changes for assignment1

--- Jon.sh
+++ Jon.sh
@@ -3,3 +3,15 @@
 # then replace all occurrences of hello with bye
 # Finally show the content of the on the screen file after the removal 
 # note:  you can use the sed command
+
+# Print name at the top
+echo "Jon Flees"
+
+# Delete line 5 from given text file
+sed '5d' ./myfile.txt > myfile2.txt
+
+# Replace all occurrences of hello with bye
+sed 's/hello/bye/g' ./myfile2.txt > myfile3.txt
+
+# Show contents after removal
+cat myfile3.txt
