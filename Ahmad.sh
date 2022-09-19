9127968637a7f05618c17a6d6a01136d17be3c28
Ahmad <ahmadreza.rezaie75@gmail.com>
2022-09-18 18:24:48 -0700

    This is Ahmad

--- Ahmad.sh
+++ Ahmad.sh
@@ -1,2 +1,10 @@
+
 # Write a bash script to print your name at the top
 # then print the contents of a file line by line using a while loop
+
+echo "Ahmad Rezaie"
+
+for line in "myfile.txt"
+do 
+	echo$line
+done
