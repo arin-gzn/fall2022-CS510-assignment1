6b0153daeae93f076be3b41f1763b1620fce29a6
Eric Adams <impactmylife1@gmail.com>
2022-09-21 17:32:32 -0700

    For HW1

--- Eric.sh
+++ Eric.sh
@@ -1,2 +1,12 @@
 # Write a bash script to print your name at the top
 # then print dates of the next 10 days after today
+echo " Eric Adams"
+set -e
+current_date=$1
+backfill_days=$2
+for ((i=0; i<$backfill_days; i++))
+do
+
+	echo "current date : $current_date"
+done
+
